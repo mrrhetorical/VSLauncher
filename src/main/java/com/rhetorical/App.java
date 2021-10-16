8ad99f4c17343a2dea9abf2d3f58a02879ed1549
Caleb Brock <caleb.brock@broshin.com>
2021-10-16 11:39:51 -0700

    Initial commit

---
+++ src/main/java/com/rhetorical/App.java
@@ -0,0 +1,13 @@
+package com.rhetorical;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
