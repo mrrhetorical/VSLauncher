14349f2161a8bb3893a585a818edac8daacb1d7d
Caleb Brock <caleb.brock@broshin.com>
2021-10-16 12:43:04 -0700

    Added basic checking of json for pack data

--- src/main/java/com/rhetorical/vslauncher/Modpack.java
+++ src/main/java/com/rhetorical/vslauncher/Modpack.java
@@ -2,9 +2,17 @@ package com.rhetorical.vslauncher;
 
 public class Modpack {
 
+	// The pack ID for identification purposes
+	public String packId;
+	// A direct link to json that identifies updates for this pack
+	public String packJson;
+	// The version of this pack
 	public String packVersion;
+	// The game version this pack uses (doesn't do anything atm)
 	public String gameVersion;
+	// The author of this pack
 	public String author;
+	// The direct download link to the .zip file containing this pack
 	public String packDownloadLink;
 
 }
--- src/main/java/com/rhetorical/vslauncher/VSLauncher.java
+++ src/main/java/com/rhetorical/vslauncher/VSLauncher.java
@@ -3,8 +3,13 @@ package com.rhetorical.vslauncher;
 import com.google.gson.Gson;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -13,11 +18,12 @@ import java.util.Scanner;
  */
 public class VSLauncher {
 
+	private static VSLauncherPrefs vsLauncherPrefs;
+	private static File installDir;
+
+	private static Map<String, Modpack> modpackMap = new HashMap<>();
+
 	public static void main( String[] args) throws Exception {
-		Scanner scanner = new Scanner(System.in);
-
-		VSLauncherPrefs vsLauncherPrefs;
-
 		File preferences = new File("vslauncherprefs.json");
 
 		if (!preferences.exists()) {
@@ -35,7 +41,7 @@ public class VSLauncher {
 			vsLauncherPrefs = gson.fromJson(reader, VSLauncherPrefs.class);
 		}
 
-		File installDir = findInstallDirectory();
+		installDir = findInstallDirectory();
 
 		if (installDir != null) {
 			System.out.println(String.format("Game install found at %s", installDir.toString()));
@@ -43,7 +49,35 @@ public class VSLauncher {
 			throw new FileNotFoundException("No game install found!");
 		}
 
+
+		updateModpackInfo();
+
+		processInput();
+	}
+
+	private static void processInput() {
+		Scanner scanner = new Scanner(System.in);
+
+
+
 		scanner.close();
+	}
+
+	private static void updateModpackInfo() throws IOException {
+
+		if (vsLauncherPrefs.modpacks != null) {
+			for (String str : vsLauncherPrefs.modpacks) {
+				URLConnection connection = new URL(str).openConnection();
+
+				Scanner scanner = new Scanner(connection.getInputStream());
+
+				String response = scanner.useDelimiter("\\A").next();
+				scanner.close();
+
+				System.out.println(String.format("Requested data from %s and received %s", str, response));
+			}
+		}
+
 	}
 
 	private static File findInstallDirectory() {
